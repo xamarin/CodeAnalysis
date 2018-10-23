f68a2cec3d377f357eb7bd663902fafb9ab779cf
Daniel Cazzulino <daniel@cazzulino.com>
2018-10-23 16:02:12 -0300

    Initial structure of projects and extension

---
+++ src/Xamarin.CodeAnalysis.Extension/XamarinCodeAnalysisPackage.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("271716A6-CCF6-47F0-A358-70869B8B24BD")]
+[assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis")]
+[assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis.Completion")]
+[assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis.Extension")]
+
+namespace Xamarin.CodeAnalysis
+
+{
+    [InstalledProductRegistration(
+        productName: "#100",
+        productDetails: "#101",
+        productId: ThisAssembly.Metadata.InformationalVersion,
+        IconResourceID = 400)]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Guid("FBE35200-65BD-42B1-AC6D-2F4EB5719209")]
+    [ProvideBindingPath]
+    public class XamarinCodeAnalysisPackage : Package
+    {
+    }
+}
