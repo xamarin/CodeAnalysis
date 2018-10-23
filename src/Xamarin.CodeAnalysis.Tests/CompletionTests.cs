5fe1e774579c3a1e42b37bb2cefbbde41e06aa5e
Daniel Cazzulino <daniel@cazzulino.com>
2018-10-23 17:00:41 -0300

    Add simple completion provider and unit test
    This showcases how to create and unit test the most basic completion provider.

---
+++ src/Xamarin.CodeAnalysis.Tests/CompletionTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Xunit;
+
+namespace Xamarin.CodeAnalysis.Tests
+{
+    public class CompletionTests
+    {
+        [Theory]
+        [InlineData(@"using System;
+public class Foo 
+{
+    public void Do() 
+    {
+        Console.WriteLine(""`"");
+    }
+}")]
+        public async Task can_retrieve_completion(string code)
+        {
+            var hostServices = MefHostServices.Create(MefHostServices.DefaultAssemblies.Concat(
+                new[]
+                {
+                    typeof(CompletionService).Assembly,
+                    typeof(StringCompletionProvider).Assembly,
+                }));
+
+            var workspace = new AdhocWorkspace(hostServices);
+            var document = workspace
+               .AddProject("TestProject", LanguageNames.CSharp)
+               .WithCompilationOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+               .WithMetadataReferences(new MetadataReference[]
+               {
+                   MetadataReference.CreateFromFile(Environment.ExpandEnvironmentVariables($@"%USERPROFILE%\.nuget\packages\netstandard.library\{ThisAssembly.Metadata.NETStandardPackageVersion}\build\netstandard2.0\ref\netstandard.dll")),
+                   MetadataReference.CreateFromFile("Xamarin.CodeAnalysis.dll"),
+                   MetadataReference.CreateFromFile("Xamarin.CodeAnalysis.Completion.dll"),
+               })
+               .AddDocument("TestDocument.cs", code);
+
+            var service = CompletionService.GetService(document);
+            Assert.NotNull(service);
+
+            var caret = code.IndexOf('`');
+            Assert.NotEqual(-1, caret);
+
+            var completions = await service.GetCompletionsAsync(document, caret);
+
+            Assert.NotNull(completions);
+            Assert.NotEmpty(completions.Items);
+        }
+    }
+}
