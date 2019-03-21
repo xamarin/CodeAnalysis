1e77b83bd90f1e6805e8826d3b02bcbb9bb58ee3
Daniel Cazzulino <daniel@cazzulino.com>
2019-03-21 17:22:43 -0300

    Make package async, fix new assembly name

--- src/Xamarin.CodeAnalysis.Windows/XamarinCodeAnalysisPackage.cs
+++ src/Xamarin.CodeAnalysis.Windows/XamarinCodeAnalysisPackage.cs
@@ -5,20 +5,19 @@ using Microsoft.VisualStudio.Shell;
 [assembly: Guid("271716A6-CCF6-47F0-A358-70869B8B24BD")]
 [assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis")]
 [assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis.Completion")]
-[assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis.Extension")]
+[assembly: ProvideCodeBase(AssemblyName = "Xamarin.CodeAnalysis.Windows")]
 
 namespace Xamarin.CodeAnalysis
-
 {
     [InstalledProductRegistration(
         productName: "#100",
         productDetails: "#101",
         productId: ThisAssembly.Metadata.InformationalVersion,
         IconResourceID = 400)]
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid("FBE35200-65BD-42B1-AC6D-2F4EB5719209")]
     [ProvideBindingPath]
-    public class XamarinCodeAnalysisPackage : Package
+    public class XamarinCodeAnalysisPackage : AsyncPackage
     {
     }
 }
