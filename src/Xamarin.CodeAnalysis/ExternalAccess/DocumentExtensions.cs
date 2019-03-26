19ff5b17d1bf0744b1215da2a56f36f3a347cc72
Daniel Cazzulino <daniel@cazzulino.com>
2019-03-26 16:41:18 -0300

    Add sample of how we can deal with internal Roslyn APIs for now
    Once our ExternalAccess PRs are merged and we have access to the resulting packages, this would not be necessary anymore.

---
+++ src/Xamarin.CodeAnalysis/ExternalAccess/DocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+// Example of how we can deal with internals for now until we get our ExternalAccess PRs merged.
+namespace Xamarin.CodeAnalysis
+{
+    static class DocumentExtensions
+    {
+        public static Task<SemanticModel> GetSemanticModelForNodeAsync(this Document document, SyntaxNode node, CancellationToken cancellationToken)
+        {
+            var extensions = typeof(Workspace).Assembly.GetType("Microsoft.CodeAnalysis.Shared.Extensions.DocumentExtensions", true);
+            var method = extensions.GetMethod(
+                nameof(GetSemanticModelForNodeAsync), 
+                BindingFlags.Public | BindingFlags.Static, 
+                null, 
+                new[] { typeof(Document), typeof(SyntaxNode), typeof(CancellationToken) }, 
+                null);
+
+            return (Task<SemanticModel>)method.Invoke(null, new object[] { document, node, cancellationToken });
+        }
+    }
+}
