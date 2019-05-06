11de2df47cc6486a674e5c1a4c86dc18f3bd2206
Juan Marcelo Tondato <jmt@xamarin.com>
2019-05-06 15:05:25 -0300

    Adds Addin for VSMAC

---
+++ poc/Xamarin.CodeAnalysis.Addin/Xamarin.CodeAnalysis.Addin/Properties/AddinInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+    "Addin", 
+    Namespace = "Xamarin.CodeAnalysis",
+    Version = "1.0"
+)]
+
+[assembly:AddinName ("Xamarin.CodeAnalysis")]
+[assembly:AddinCategory ("Code Analysis")]
+[assembly:AddinDescription ("Code Analysis for Xamarin Projects")]
+[assembly:AddinAuthor ("xamarin")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
