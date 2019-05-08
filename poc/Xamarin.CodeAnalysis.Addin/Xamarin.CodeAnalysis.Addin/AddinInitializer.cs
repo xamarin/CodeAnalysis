a740bf5838b6635d7ec594bab71813305c091591
Juan Marcelo Tondato <jmt@xamarin.com>
2019-05-08 14:32:48 -0300

    Adds hack to include analysers to MEF

---
+++ poc/Xamarin.CodeAnalysis.Addin/Xamarin.CodeAnalysis.Addin/AddinInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Xamarin.CodeAnalysis.Addin
+{
+    public class AddinInitializer
+    {
+        public AddinInitializer()
+        {
+            // HACK: REQUIRED TO UPDATE MEF COMPONENTS WITH THE CLASSES FROM THIS DLL
+            if (MonoDevelop.Ide.IdeApp.IsInitialized) { }
+        }
+    }
+}
