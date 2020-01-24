00cbaee5cf0b6cec5e4329b60f89129bd8a03367
Whitney Schmidt <whschm@microsoft.com>
2020-01-24 13:35:47 -0500

    add xamarin.macios attribute analyzer files + (broken) build logic

---
+++ src/Xamarin.CodeAnalysis/AttributeAnalyzer.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using System.Xml.Linq;
+using AttributeAnalyzer.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using Xamarin.MacDev;
+
+namespace AttributeAnalyzer {
+	[DiagnosticAnalyzer (LanguageNames.CSharp)]
+	public class AttributeAnalyzerAnalyzer : DiagnosticAnalyzer {
+		const string ConfigFileName = "Info.plist";
+		const string Category = "Notifications";
+
+		static readonly DiagnosticDescriptor adviceRule = new DiagnosticDescriptor (
+		    "XI0001",
+		    "Notifies you with advice on how to use Apple APIs",
+		    "{0}",
+		    Category,
+		    DiagnosticSeverity.Info,
+		    true
+		);
+
+		static readonly DiagnosticDescriptor introducedRule = new DiagnosticDescriptor (
+		    "XI0002",
+		    "Notifies you if you are using newer Apple APIs when targeting an older OS version",
+		    "{0}",
+		    Category,
+		    DiagnosticSeverity.Info,
+		    true,
+		    "This rule is comparing the versions of the API's introduced attribute and the minimum deployment target defined in the Info.plist."
+		);
+
+		static readonly DiagnosticDescriptor deprecatedRule = new DiagnosticDescriptor (
+		    "XI0003",
+		    "Notifies you when using a deprecated, obsolete or unavailable Apple API",
+		    "{0}",
+		    Category,
+		    DiagnosticSeverity.Info,
+		    true
+		);
+
+		static readonly ImmutableArray<OperationKind> operationKindsOfInterest = ImmutableArray.Create (
+		    OperationKind.EventReference,
+		    OperationKind.FieldReference,
+		    OperationKind.Invocation,
+		    OperationKind.MethodReference,
+		    OperationKind.PropertyReference,
+		    OperationKind.ObjectCreation
+		);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (adviceRule, introducedRule, deprecatedRule);
+
+		const string AdviceAttribute = "Foundation.AdviceAttribute";
+
+		public override void Initialize (AnalysisContext context)
+		{
+			context.EnableConcurrentExecution ();
+			context.ConfigureGeneratedCodeAnalysis (GeneratedCodeAnalysisFlags.None);
+			context.RegisterCompilationStartAction (OnCompilationStart);
+		}
+
+		PlatformName GetPlatform (IEnumerable<AssemblyIdentity> referencedAssemblyNames)
+		{
+			foreach (var element in referencedAssemblyNames) {
+				switch (element.Name) {
+				case "Xamarin.iOS":
+					return PlatformName.iOS;
+				case "Xamarin.WatchOS":
+					return PlatformName.WatchOS;
+				case "Xamarin.TVOS":
+					return PlatformName.TvOS;
+				case "Xamarin.Mac":
+					return PlatformName.MacOSX;
+				}
+			}
+			return PlatformName.None;
+		}
+
+		void OnCompilationStart (CompilationStartAnalysisContext compilationContext)
+		{
+            
+          AdditionalText infoPlistFilePath = compilationContext.Options.AdditionalFiles.FirstOrDefault (f => Path.GetFileName (f.Path) == ConfigFileName);
+
+            if (infoPlistFilePath == null)
+                return;
+			var adviceAttribute = compilationContext.Compilation.GetTypeByMetadataName (AdviceAttribute);
+			if (adviceAttribute == null)
+				return;
+
+			var referencedAssemblyNames = compilationContext.Compilation.ReferencedAssemblyNames;
+
+			PlatformName platformType = GetPlatform (referencedAssemblyNames);
+
+			compilationContext.RegisterOperationAction (
+			    (nodeContext) => {
+
+				    var symbol = nodeContext.Operation.GetSymbol ();
+				    if (symbol == null)
+					    return;
+
+				    if (!TryGetSymbolMergedAttributes (nodeContext, symbol, adviceAttribute, out PlatformAvailability availability) &&
+			    !TryGetSymbolMergedAttributes (nodeContext, symbol.ContainingType, adviceAttribute, out availability))
+					    return;
+
+				    var message = availability.GetDeprecatedMessage (platformType);
+
+				    Version minimumOSVersion = GetMinimumDeploymentTargetNumber (infoPlistFilePath);
+
+				    bool introduced = false;
+				    if (message == null) {
+					    message = availability.GetMinimumRequiredPlatformMessage (minimumOSVersion, platformType);
+					    introduced = true;
+				    }
+
+				    if (message == null)
+					    return;
+
+				    if (availability.Message != null)
+					    message = string.Format ("{0}.\n {1}", message, availability.Message);
+
+				    AddIssue (nodeContext, introduced ? introducedRule : deprecatedRule, string.Format ("'{0}' {1}", symbol.Name, message));
+			    },
+			    operationKindsOfInterest
+			);
+		}
+
+		bool TryGetSymbolMergedAttributes (OperationAnalysisContext context, ISymbol symbol, INamedTypeSymbol adviceAttribute, out PlatformAvailability availability)
+		{
+			availability = null;
+
+			MergeSymbolAttributes (context, symbol, adviceAttribute, ref availability);
+
+			if (symbol is IPropertySymbol property) {
+				var syntax = context.Operation.Syntax;
+				var parent = syntax?.Parent;
+				IMethodSymbol method = null;
+
+				switch(parent) {
+				case AssignmentExpressionSyntax assignment:
+					method = (assignment.Right == syntax) ? property.GetMethod : property.SetMethod;
+					break;
+				case PostfixUnaryExpressionSyntax _:
+				case PrefixUnaryExpressionSyntax _:
+					if ((method = property.GetMethod) != null)
+						MergeSymbolAttributes (context, method, adviceAttribute, ref availability);
+
+					method = property.SetMethod;
+					break;
+				case ElementAccessExpressionSyntax _:
+				case ArgumentSyntax _:
+					method = property.GetMethod;
+					break;
+				}
+
+				if (method != null)
+					MergeSymbolAttributes (context, method, adviceAttribute, ref availability);
+			}
+
+			return availability != null && availability.IsSpecified;
+		}
+
+		void MergeSymbolAttributes (OperationAnalysisContext context, ISymbol symbol, INamedTypeSymbol adviceAttribute, ref PlatformAvailability availability)
+		{
+			foreach (var attr in symbol.GetAttributes ()) {
+				if (attr.AttributeClass == adviceAttribute && attr.ConstructorArguments.Length > 0) {
+					AddIssue (context, adviceRule, attr.ConstructorArguments [0].Value?.ToString ());
+				} else {
+					availability = availability.Merge (attr);
+				}
+			}
+		}
+
+		void AddIssue (OperationAnalysisContext context, DiagnosticDescriptor rule, string message) => context.ReportDiagnostic (Diagnostic.Create (rule, context.Operation.Syntax.GetLocation (), message));
+
+		static string GetMinimumOSVersionLine (AdditionalText infoPlistFilePath)
+		{
+			PDictionary dict = PDictionary.FromFile (infoPlistFilePath.Path.ToString ());
+
+			if (dict.TryGetValue ("MinimumOSVersion", out PString value) || dict.TryGetValue ("LSMinimumSystemVersion", out value))
+				return value.Value;
+
+			return String.Empty; // should never reach here; placeholder value just in case
+		}
+
+		static Version GetMinimumDeploymentTargetNumber (AdditionalText infoPlistFilePath)
+		{
+			var minimumOSNumber = GetMinimumOSVersionLine (infoPlistFilePath);
+
+			Version.TryParse (minimumOSNumber, out Version vers);
+			return vers;
+		}
+	}
+}
+
+namespace AttributeAnalyzer.Extensions {
+
+	public static class MyExtensions {
+
+		public static ISymbol GetSymbol (this IOperation operation)
+		{
+			switch (operation) {
+			case IMemberReferenceOperation memberReference:
+				return memberReference.Member;
+			case IInvocationOperation invocation:
+				return invocation.TargetMethod;
+			case IObjectCreationOperation creation:
+				return creation.Constructor;
+			default:
+				return null;
+			}
+		}
+	}
+}
---
+++ src/Xamarin.CodeAnalysis/PlatformAvailabilityExtensions.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.IO;
+
+namespace AttributeAnalyzer {
+	public static class PlatformAvailabilityExtensions {
+		public static PlatformAvailability Merge (this PlatformAvailability availability, AttributeData attr)
+		{
+			if (attr.AttributeClass.ContainingNamespace.Name != "ObjCRuntime")
+				return availability;
+
+
+			switch (attr.AttributeClass.Name) {
+			case "iOSAttribute":
+				availability = MergeShorthandAttribute (availability, attr, false);
+				break;
+			case "MacAttribute":
+				availability = MergeShorthandAttribute (availability, attr, true);
+				break;
+			case "AvailabilityAttribute": // all of the following attributes subclass this
+				availability = MergeAvailabilityAttribute (availability, attr);
+				break;
+			case "IntroducedAttribute":
+				availability = MergeNewStyleAvailabilityAttribute (availability, attr, AvailabilityKind.Introduced);
+				break;
+			case "DeprecatedAttribute":
+				availability = MergeNewStyleAvailabilityAttribute (availability, attr, AvailabilityKind.Deprecated);
+				break;
+			case "ObsoletedAttribute":
+				availability = MergeNewStyleAvailabilityAttribute (availability, attr, AvailabilityKind.Obsoleted);
+				break;
+			case "UnavailableAttribute":
+				availability = MergeNewStyleAvailabilityAttribute (availability, attr, AvailabilityKind.Unavailable);
+				break;
+			}
+
+			return availability;
+		}
+
+		static PlatformAvailability MergeNewStyleAvailabilityAttribute (PlatformAvailability availability, AttributeData attr, AvailabilityKind availabilityKind)
+		{
+			var platformName = (PlatformName)(byte)attr.ConstructorArguments [0].Value;
+			var platformSet = new PlatformSet ();
+			Platform platform = platformSet.GetPlatform (platformName);
+			if (platform == null)
+				throw new ArgumentOutOfRangeException ($"Platform name: {platformName}.");
+
+			if (availability == null)
+				availability = new PlatformAvailability ();
+
+			switch (attr.ConstructorArguments.Length) {
+			case 3: //PlatformName platform, PlatformArchitecture architecture, string message
+				platform.Architecture = (PlatformArchitecture)(byte)attr.ConstructorArguments [1].Value;
+				break;
+			case 5: //PlatformName platform, int majorVersion, int minorVersion, PlatformArchitecture architecture, string message
+				platform.Major = (byte)(int)attr.ConstructorArguments [1].Value;
+				platform.Minor = (byte)(int)attr.ConstructorArguments [2].Value;
+				platform.Architecture = (PlatformArchitecture)(byte)attr.ConstructorArguments [3].Value;
+				availability.Message = (string)attr.ConstructorArguments [4].Value;
+				break;
+			case 6: //PlatformName platform, int majorVersion, int minorVersion, int subminorVersion, PlatformArchitecture architecture, string message
+				platform.Major = (byte)(int)attr.ConstructorArguments [1].Value;
+				platform.Minor = (byte)(int)attr.ConstructorArguments [2].Value;
+				platform.Minor = (byte)(int)attr.ConstructorArguments [3].Value;
+				platform.Architecture = (PlatformArchitecture)(byte)attr.ConstructorArguments [4].Value;
+				availability.Message = (string)attr.ConstructorArguments [5].Value;
+				break;
+			}
+
+			switch (availabilityKind) {
+			case AvailabilityKind.Introduced:
+				availability.Introduced |= platformSet;
+				break;
+			case AvailabilityKind.Deprecated:
+				availability.Deprecated |= platformSet;
+				break;
+			case AvailabilityKind.Obsoleted:
+				availability.Obsoleted |= platformSet;
+				break;
+			case AvailabilityKind.Unavailable:
+				availability.Unavailable |= platformSet;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException ();
+			}
+
+			return availability;
+		}
+
+		static PlatformAvailability MergeShorthandAttribute (PlatformAvailability availability,
+									     AttributeData attr, bool isMac, byte legacyNamedMacAttrMinorVersion = 0)
+		{
+			if (availability == null)
+				availability = new PlatformAvailability ();
+
+			if (availability.Introduced == null)
+				availability.Introduced = new PlatformSet ();
+
+			var platform = isMac
+			    ? availability.Introduced.MacOSX
+			    : availability.Introduced.iOS;
+
+			var onlyOn64ArgIndex = 2;
+
+			if (legacyNamedMacAttrMinorVersion > 0) {
+				onlyOn64ArgIndex = 0;
+				platform.Major = 10;
+				platform.Minor = legacyNamedMacAttrMinorVersion;
+			} else {
+				platform.Major = (byte)attr.ConstructorArguments [0].Value;
+				platform.Minor = (byte)attr.ConstructorArguments [1].Value;
+			}
+
+			if (attr.ConstructorArguments.Length == onlyOn64ArgIndex + 1 &&
+			    (bool)attr.ConstructorArguments [onlyOn64ArgIndex].Value)
+				platform.Architecture = PlatformArchitecture.Arch64;
+
+			return availability;
+		}
+
+		static PlatformAvailability MergeAvailabilityAttribute (PlatformAvailability availability, AttributeData attr)
+		{
+			if (availability == null)
+				availability = new PlatformAvailability ();
+
+			// first read positional arguments as they come first syntatically
+			int n = Math.Min (attr.ConstructorArguments.Length, 4);
+
+			for (int i = 0; i < n; i++) {
+				var value = (ulong)attr.ConstructorArguments [i].Value;
+				if (value == 0)
+					continue;
+
+				var platform = new PlatformSet (value);
+
+				switch (i) {
+				case 0:
+					availability.Introduced |= platform;
+					break;
+				case 1:
+					availability.Deprecated |= platform;
+					break;
+				case 2:
+					availability.Obsoleted |= platform;
+					break;
+				case 3:
+					availability.Unavailable |= platform;
+					break;
+				}
+			}
+
+			// (e.g. [Availability (Platform.Mac_10_9, Introduced = Platform.Mac_10_10)]
+			// makes no sense, but could technically happen, in which case Platform.Mac_10_10
+			// would be the winner).
+			foreach (var named in attr.NamedArguments) {
+				switch (named.Key) {
+				case "Introduced":
+					availability.Introduced |= new PlatformSet ((ulong)named.Value.Value);
+					break;
+				case "Deprecated":
+					availability.Deprecated |= new PlatformSet ((ulong)named.Value.Value);
+					break;
+				case "Obsoleted":
+					availability.Obsoleted |= new PlatformSet ((ulong)named.Value.Value);
+					break;
+				case "Unavailable":
+					availability.Unavailable |= new PlatformSet ((ulong)named.Value.Value);
+					break;
+				case "Message":
+					string message = named.Value.Value as string;
+
+					if (message != null) // maybe throw an exception here instead of silently skipping this?
+						availability.Message += (availability.Message == null) ? message : string.Join ("; ", message);
+
+					break;
+				}
+			}
+
+			return availability;
+		}
+
+		public static string GetMinimumRequiredPlatformMessage (this PlatformAvailability availability, Version minOSVersion, PlatformName platformName)
+		{
+			if (availability == null || !availability.IsSpecified || platformName == PlatformName.None)
+				return null;
+
+			var targetPlatform = GetDeploymentTargetPlatform (minOSVersion, platformName); // used to get Project project passed in
+			if (targetPlatform == null)
+				return null;
+
+			var introduced = availability.GetIntroducedPlatformFor (targetPlatform);
+			if (introduced == null || !introduced.IsSpecified || introduced.CompareTo (targetPlatform) <= 0)
+				return null;
+
+			if (introduced.Architecture != PlatformArchitecture.All &&
+				introduced.Architecture.HasFlag (PlatformArchitecture.Arch64))
+				return $"is only available on {introduced.FullName} or newer and requires a 64-bit architecture";
+			else
+				return $"is only available on {introduced.FullName} or newer";
+		}
+
+		static Boolean IsAttributeOfCurrentPlatform (PlatformSet platformSet, PlatformName platformName)
+			=> (platformSet == null) ? false : platformSet.IsSpecifiedFor (platformName);
+
+		static string GetFullName (PlatformSet platformSet, PlatformName platformName) =>
+			(platformSet != null) ? platformSet.GetFullName (platformName) : throw new ArgumentNullException();
+
+		public static string GetDeprecatedMessage (this PlatformAvailability availability, PlatformName platformName)
+		{
+			if (availability == null || !availability.IsSpecified || platformName == PlatformName.None)
+				return null;
+
+			var unavailable = IsAttributeOfCurrentPlatform (availability.Unavailable, platformName);
+			var obsoleted = IsAttributeOfCurrentPlatform (availability.Obsoleted, platformName);
+			var deprecated = IsAttributeOfCurrentPlatform (availability.Deprecated, platformName);
+
+			if (unavailable)
+				return $"was made unavailable in {GetFullName (availability.Unavailable, platformName)}";
+
+			if (obsoleted && deprecated)
+				return $"was deprecated in {GetFullName (availability.Deprecated, platformName)} and obsoleted (might be removed) in {GetFullName (availability.Obsoleted, platformName)}";
+
+			if (obsoleted)
+				return $"was obsoleted (might be removed) in {GetFullName (availability.Obsoleted, platformName)}";
+
+			if (deprecated)
+				return $"was deprecated in {GetFullName (availability.Deprecated, platformName)}";
+
+			return null;
+		}
+
+		static Platform GetDeploymentTargetPlatform (Version minOSVersion, PlatformName platformName)
+			=>  new Platform (
+			    platformName,
+			    major: (byte)minOSVersion.Major,
+			    minor: (byte)minOSVersion.Minor
+			);
+	}
+}
