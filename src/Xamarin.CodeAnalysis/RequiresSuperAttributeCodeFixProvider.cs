fa0827d50edf7dd4fced06ba3335dda75724d402
Madeline Zhang <maddyz@mit.edu>
2020-01-29 17:39:19 -0500

    Update src/Xamarin.CodeAnalysis/RequiresSuperAttributeCodeFixProvider.cs
    Co-Authored-By: Manuel de la Pena <mandel@microsoft.com>

--- src/Xamarin.CodeAnalysis/RequiresSuperAttributeCodeFixProvider.cs
+++ src/Xamarin.CodeAnalysis/RequiresSuperAttributeCodeFixProvider.cs
@@ -53,7 +53,7 @@ namespace RequiresSuperAttribute
 		  CancellationToken cancellationToken)
 		{
 			//Find the node to fix.
-			var semanticModel = await document.GetSemanticModelAsync (cancellationToken);
+			var semanticModel = await document.GetSemanticModelAsync (cancellationToken).ConfigureAwait (false);
 			var memberSymbol = semanticModel.GetDeclaredSymbol (methodDeclaration) as IMethodSymbol;
 
 			//Generate the new base.[methodName]([arguments]) block. If the method needs to return the base call,
