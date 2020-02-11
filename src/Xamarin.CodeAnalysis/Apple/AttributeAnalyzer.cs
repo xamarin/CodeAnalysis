70e0edcde9d0406e52104a23af6eda562aa09994
Whitney Schmidt <whschm@microsoft.com>
2020-02-11 15:03:54 -0500

    Revert "playing around with filtering out [RequiresSuper] duplicate issues"
    This reverts commit 9a935490975bf7f62d265f66060f4cef23dd5a85.

--- src/Xamarin.CodeAnalysis/Apple/AttributeAnalyzer.cs
+++ src/Xamarin.CodeAnalysis/Apple/AttributeAnalyzer.cs
@@ -186,12 +186,7 @@ namespace Xamarin.CodeAnalysis.Apple
             {
                 if (attr.AttributeClass == adviceAttribute && attr.ConstructorArguments.Length > 0)
                 {
-                    var message = attr.ConstructorArguments[0].Value?.ToString();
-
-                    if (message.Equals("Overriding this method requires a call to the overriden method."))
-                        continue;
-
-                    AddIssue(context, adviceRule, message);
+                    AddIssue(context, adviceRule, attr.ConstructorArguments[0].Value?.ToString());
                 }
                 else
                 {
